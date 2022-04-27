cb303192f560de41f03c12e452b00dbee9551876
2214962083 <2214962083@qq.com>
2022-04-28 01:18:39 +0800

    fix: fix test bug, update github actions

--- packages/class-mock/src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import {say} from './index'
-
-describe('class-mock', () => {
-  test('say', () => {
-    expect(say()).toBe('hello mock')
-  })
-})
--- packages/class-mock/src/index.ts
+++ packages/class-mock/src/index.ts
@@ -1,3 +1,16 @@
-export const say = () => 'hello mock'
+import 'reflect-metadata'
 
-say()
+@Reflect.metadata('type', 'class')
+class A {
+  constructor(public name: string, public age: number) {}
+  @Reflect.metadata(undefined, undefined)
+  method(): boolean {
+    return true
+  }
+}
+
+const t1 = Reflect.getMetadata('design:paramtypes', A)
+const t2 = Reflect.getMetadata('design:returntype', A.prototype, 'method')
+const t3 = Reflect.getMetadata('design:type', A.prototype, 'method')
+
+console.log(...t1, t2, t3)
